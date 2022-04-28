03dadc7b8bad05ead9b51fa10c9dd40b855c65d2
Oleksandr Borymskiy <favorstreet21@gmail.com>
2022-04-29 00:05:11 +0300

    fix: error correction

--- src/js/app.js
+++ src/js/app.js
@@ -21,10 +21,10 @@ const message = 'Developed by Glivera-team https://glivera-team.com/';
 console.info('%c%s', styles, message);
 
 // -------------------  dev widget
-if (GLOBAL_VARS.projectDevStatus) { // TODO: widget
-	pageWidgetInit();
-	console.log(process.env.NODE_ENV);
-}
+// if (GLOBAL_VARS.projectDevStatus) { // TODO: widget
+// 	pageWidgetInit();
+// 	console.log(process.env.NODE_ENV);
+// }
 // -------------------  dev widget###
 
 // -------------------  global variables
